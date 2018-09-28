712a51a64d54093921c8756983822522c8bbb4cb
bolliger32 <bolliger32@local>
2018-09-28 15:23:14 +0000

    fix multilayer directory bug in cp_to_gcs

--- rhg_compute_tools/gcs.py
+++ rhg_compute_tools/gcs.py
@@ -30,6 +30,29 @@ def get_bucket(cred_path):
     return bucket
 
 
+def _cp_dir_to_gcs(bucket,src,dest):
+    '''Recursively copy a directory from local path to GCS'''
+    
+    files_loc = glob(join(src,'*'))
+    if src[-1] != '/': src += '/'
+
+    # upload directory blob
+    if bucket.get_blob(dest) is None:
+        newblob = bucket.blob(dest)
+        newblob.upload_from_string('')
+
+    # upload file blobs
+    for f_loc in files_loc:
+        fname = basename(f_loc)
+        this_dest = join(dest,fname)
+        if isfile(f_loc):
+            newblob = bucket.blob(this_dest)
+            newblob.upload_from_filename(f_loc)
+        # if directory, call this recursively
+        else:
+            _cp_dir_to_gcs(bucket,f_loc,this_dest)
+
+
 def cp_to_gcs(src, dest, cred_path='/opt/gcsfuse_tokens/rhg-data.json'):
     '''Copy a file or recursively copy a directory from local
     path to GCS.
@@ -41,6 +64,7 @@ def cp_to_gcs(src, dest, cred_path='/opt/gcsfuse_tokens/rhg-data.json'):
     dest : str
         If copying a directory, this is the path of the directory blob on GCS.
         If copying a file, this is the path of the file blob on GCS.
+        This path begins with e.g. 'impactlab-rhg/...'.
     cred_path (optional) : str
         Path to credentials file. Default is the default location on Rhg workers.
         
@@ -59,19 +83,7 @@ def cp_to_gcs(src, dest, cred_path='/opt/gcsfuse_tokens/rhg-data.json'):
         newblob.upload_from_filename(src)
 
     else:
-        files_loc = glob(join(src,'*'))
-        if src[-1] != '/': src += '/'
-            
-        # upload directory blob
-        if bucket.get_blob(dest) is None:
-            newblob = bucket.blob(dest)
-            newblob.upload_from_string('')
-        
-        # upload file blobs
-        for f_loc in files_loc:
-            fname = basename(f_loc)
-            newblob = bucket.blob(join(dest,fname))
-            newblob.upload_from_filename(f_loc)
+        _cp_dir_to_gcs(bucket, src, dest)
             
     return dt.now() - st_time
         
