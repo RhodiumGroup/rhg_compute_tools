2f4fcbe78859142c95136147120c70f475544dde
Michael Delgado <delgado.michaelt@gmail.com>
2018-10-08 10:53:06 -0700

    add flavored cluster cretation helpers

--- rhg_compute_tools/kubernetes.py
+++ rhg_compute_tools/kubernetes.py
@@ -19,11 +19,27 @@ def traceback(ftr):
     str : Traceback
     """
     return tb.print_exception(
-    type(ftr.exception()),
-    ftr.exception(),
-    ftr.traceback())
+        type(ftr.exception()),
+        ftr.exception(),
+        ftr.traceback())
 
-def get_worker(
+
+def append_docstring(func_with_docstring):
+    def decorator(func):
+        if func.__doc__ is None:
+            func.__doc__ = ''
+        if func_with_docstring.__doc__ is None:
+            func.__doc__ = ''
+
+        func.__doc__ = (
+            func.__doc__
+            + '\n'.join(func_with_docstring.__doc__.lstrip().split('\n')[1:]))
+
+        return func
+    return decorator
+
+
+def get_cluster(
         name=None,
         extra_pip_packages=None,
         extra_conda_packages=None,
@@ -31,18 +47,22 @@ def get_worker(
         nthreads=None,
         cpus=None,
         cred_path=None,
-        env_items=None):
+        env_items=None,
+        scaling_factor=1):
     """
     Start dask.kubernetes cluster and dask.distributed client
+
+    All arguments are optional. If not provided, arguments will default to
+    values provided in ``~/worker-template.yml``.
 
     Parameters
     ----------
     name : str, optional
         Name of worker image to use. If None, default to worker specified in
-        `/home/jovyan/worker-template.yml`.
+        ``/home/jovyan/worker-template.yml``.
     extra_pip_packages : str, optional
         Extra pip packages to install on worker. Syntax to install is
-        "pip install `extra_pip_packages`"
+        ``pip install extra_pip_packages``
     extra_conda_packages :str, optional
         Same as above except for conda
     memory_gb : float, optional
@@ -53,17 +73,39 @@ def get_worker(
         should ever be set to something other than 1.
     cpus : float, optional
         Number of virtual CPUs to assign per 'group of workers'
-    cred_path : str or None, optional
+    cred_path : str, optional
         Path to Google Cloud credentials file to use.
     env_items : list of dict, optional
-        A list of env variable 'name'-'value' pairs to append to the env variables
-        included in worker-template.yml. (e.g. [{'name': 'GCLOUD_DEFAULT_TOKEN_FILE',
-        'value': '/opt/gcsfuse_tokens/rhg-data.json'}])
+        A list of env variable 'name'-'value' pairs to append to the env
+        variables included in ``~/worker-template.yml``, e.g.
+
+        .. code-block:: python
+
+            [{
+                'name': 'GCLOUD_DEFAULT_TOKEN_FILE',
+                'value': '/opt/gcsfuse_tokens/rhg-data.json'}])
+
+    scaling_factor: float, optional
+        scale the worker memory & CPU size using a constant multiplier of the
+        specified worker. No constraints in terms of performance or cluster
+        size are enforced - if you request too little the dask worker will not
+        perform; if you request too much you may see an ``InsufficientMemory``
+        or ``InsufficientCPU`` error on the google cloud Kubernetes console.
+
+        Recommended scaling factors given our default ``~/worker-template.yml``
+        specs are [0.5, 1, 2, 4].
 
     Returns
     -------
-    client : :py:class:dask.distributed.Client
-    cluster : :py:class:dask_kubernetes.KubeCluster
+    client : object
+        :py:class:`dask.distributed.Client` connected to cluster
+    cluster : object
+        Pre-configured :py:class:`dask_kubernetes.KubeCluster`
+
+    See Also
+    --------
+    :py:func:`get_micro_cluster`, :py:func:`get_standard_cluster`,
+    :py:func:`get_big_cluster`, :py_func:`get_giant_cluster`
     """
 
     with open('/home/jovyan/worker-template.yml', 'r') as f:
@@ -76,14 +118,20 @@ def get_worker(
         container['image'] = name
 
     if extra_pip_packages is not None:
-        container['env'].append({'name':'EXTRA_PIP_PACKAGES',
-                                        'value':extra_pip_packages})
+        container['env'].append({
+            'name': 'EXTRA_PIP_PACKAGES',
+            'value': extra_pip_packages})
+
     if extra_conda_packages is not None:
-        container['env'].append({'name':'EXTRA_CONDA_PACKAGES',
-                                        'value':extra_conda_packages})
+        container['env'].append({
+            'name': 'EXTRA_CONDA_PACKAGES',
+            'value': extra_conda_packages})
+
     if cred_path is not None:
-        container['env'].append({'name': 'GCLOUD_DEFAULT_TOKEN_FILE',
-                                 'value': cred_path})
+        container['env'].append({
+            'name': 'GCLOUD_DEFAULT_TOKEN_FILE',
+            'value': cred_path})
+
     if env_items is not None:
         container['env'] = container['env'] + env_items
 
@@ -119,3 +167,39 @@ def get_worker(
     client = dd.Client(cluster)
 
     return client, cluster
+
+
+@append_docstring(get_cluster)
+def get_giant_cluster(*args, **kwargs):
+    """
+    Start a worker with 4x the memory and CPU of the specified worker
+    """
+
+    return get_cluster(*args, scaling_factor=4, **kwargs)
+
+
+@append_docstring(get_cluster)
+def get_big_cluster(*args, **kwargs):
+    """
+    Start a worker with 2x the memory and CPU of the specified worker
+    """
+
+    return get_cluster(*args, scaling_factor=2, **kwargs)
+
+
+@append_docstring(get_cluster)
+def get_standard_cluster(*args, **kwargs):
+    """
+    Start a worker with 1x the memory and CPU of the specified worker
+    """
+
+    return get_cluster(*args, scaling_factor=1, **kwargs)
+
+
+@append_docstring(get_cluster)
+def get_micro_cluster(*args, **kwargs):
+    """
+    Start a worker with 0.5x the memory and CPU of the specified worker
+    """
+
+    return get_cluster(*args, scaling_factor=0.5, **kwargs)
