d2f0dd7d630b930c6bb3e9d41593613ea855f247
Michael Delgado <delgado.michaelt@gmail.com>
2018-10-09 20:16:28 -0700

    bumped version to v0.1.4

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ test_requirements = [
 
 setup(
     name='rhg_compute_tools',
-    version='0.1.3',
+    version='v0.1.4',
     description="Tools for using compute.rhg.com and compute.impactlab.org",
     long_description=readme + '\n\n' + history,
     author="Michael Delgado",
