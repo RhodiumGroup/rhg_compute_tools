3f9ecf97118c8228e7868e428f8a025c25a8c18a
Michael Delgado <delgado.michaelt@gmail.com>
2020-05-31 13:23:11 -0700

    import fixtures.py relative to package root

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -7,7 +7,7 @@ import pytest
 import click
 from click.testing import CliRunner
 import rhg_compute_tools.cli
-from fixtures import (
+from tests.fixtures import (
     replicate_directory_structure_on_gcs_stub,
     tempfl_path
 )
