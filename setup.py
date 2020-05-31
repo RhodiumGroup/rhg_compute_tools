e2ce628cdd3d4e8f7b3ba2a17ba66d49dc2eb75b
Michael Delgado <delgado.michaelt@gmail.com>
2020-05-31 14:37:41 -0700

    update testing spec to py 3.7 and 3.8, pypi classifiers

--- setup.py
+++ setup.py
@@ -47,15 +47,13 @@ setup(
     zip_safe=False,
     keywords='rhg_compute_tools',
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
-        "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6'
+        'Programming Language :: Python :: 3.7'
+        'Programming Language :: Python :: 3.8'
     ],
     test_suite='tests',
     tests_require=test_requirements,
