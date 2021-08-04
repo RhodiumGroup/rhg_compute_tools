f936930f2cf04bddd1770a838763136bea1a26f8
Michael Delgado <delgadom@users.noreply.github.com>
2021-08-04 16:41:31 -0700

    Update __init__.py

--- rhg_compute_tools/__init__.py
+++ rhg_compute_tools/__init__.py
@@ -7,4 +7,10 @@ import pkg_resources
 
 __author__ = """Michael Delgado"""
 __email__ = "mdelgado@rhg.com"
-__version__ = pkg_resources.get_distribution("rhg_compute_tools").version
+
+try:
+    __version__ = pkg_resources.get_distribution("rhg_compute_tools").version    
+except Exception:
+    # Local copy or not installed with setuptools.
+    # Disable minimum version checks on downstream libraries.
+    __version__ = "999"
